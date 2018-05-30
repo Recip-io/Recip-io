160837ff388e1ed811f197f3bc43c7517517ec6c
Heshie London <heshielondon@gmail.com>
2018-05-30 16:13:14 -0700

    db schemas for user and recipio

--- db/index.js
+++ db/index.js
@@ -10,3 +10,53 @@ db.on('error', function() {
 db.once('open', function() {
   console.log('mongoose connected successfully');
 });
+
+// schema for new users
+let userSchema = mongoose.Schema({
+  username: String,
+  email: string,
+  bio: { about: string, location: string },
+  my_recipios: [],
+  favorites: [],
+  hash: string,
+  salt: string
+})
+
+let User = mongooose.model('User', userSchema);
+
+var user = new User();
+
+user.save(function(err, user) {
+  if (err) {
+    console.error(err, null);
+  }
+  console.log(null, user);
+});
+
+// schema for user created recipios
+let recipioSchema = mongoose.Schema({
+  name: String,
+  created_by: String,
+  description: String,
+  tags: [''],
+  image_url: String,
+  recipe: String,
+  comments: [{ comment_body: String, date: Date }],
+  votes: Number,
+  favs:  Number,
+  shares: Number
+})
+
+let Recipio = mongoose.model('Recipio', recipioSchema);
+
+var recipio = new Recipio();
+
+recipio.save(function(err, recipio) {
+  if (err) {
+    console.error(err, null);
+  }
+  console.log(null, recipio);
+});
+
+module.exports.saveUser = user.save;
+module.exports.saveRecipio = recipio.save;
