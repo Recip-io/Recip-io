f358b0a605b835b72c5cd5341b2186f28b6bb763
Heshie London <heshielondon@gmail.com>
2018-06-09 14:54:00 -0700

    init of local search component

---
+++ client/src/components/SearchDB.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchDB = (props) => (
+  <div className="search-bar">
+
+  </div>
+);
+
+export default SearchDB;
+
+    // <form method="POST" onSubmit={}>
+    //   <input type="text" placeholder="Search your Recipios" id="searchdb" name="recipeName" value={} onChange={}></input>
+    // </form>
