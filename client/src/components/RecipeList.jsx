e5e2381743bc2f6665b31c1ff772bd3b99626b29
Heshie London <heshielondon@gmail.com>
2018-06-10 14:05:54 -0700

    passes on update function in props

--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -4,7 +4,7 @@ import Recipe from './Recipe.jsx';
 const RecipeList = (props) => (
   <div className="recipe-list">
     <ul>
-        {props.recipes.map((recipe) => <Recipe recipe={recipe} handleRecipeTitleOrImageClick={props.handleRecipeTitleOrImageClick}/>)}
+        {props.recipes.map((recipe) => <Recipe recipe={recipe} handleRecipeTitleOrImageClick={props.handleRecipeTitleOrImageClick} componentDidMount={props.componentDidMount}/>)}
     </ul>
   </div>
 );
