015c882d62ee4a2ff91f593a513d5e696a9f4258
Heshie London <heshielondon@gmail.com>
2018-06-10 14:04:54 -0700

    fav click function working

--- client/src/components/Recipe.jsx
+++ client/src/components/Recipe.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
+import $ from 'jquery';
 
 
-var favIncrementer = function(props) {
-  var blogId = props.recipe._id;
-  $.patch( "/api/recipios/' + blogId'");
+var favPlusOne = function(props) {
+  event.preventDefault();
+  var recipioId = JSON.stringify({ _id: props.recipe._id });
+  console.log('in favPlusOne, recipioId = ', recipioId);
+  $.ajax({
+    method: 'POST',
+    url: '/api/recipios/fav',
+    data: recipioId,
+    contentType: 'application/json'
+  })
+    .done(function(data) {
+      console.log('favs incremented successfully', data);
+      props.componentDidMount();
+    })
+    .fail(function() {
+      console.log('favs failed increment');
+  });
 }
 
 const Recipe = (props) => (
-  <li className="recipe-list-entry" key={props.recipe._id} onChange={() => favIncrementer(props)}>
+  <li className="recipe-list-entry" key={props.recipe._id}>
     <div className="recipe">
-      <h1 className="recipe-title" onClick={() => props.handleRecipeTitleOrImageClick(props.recipe)}>{props.recipe.recipeName}</h1>
+      <span className="recipe-title" onClick={() => props.handleRecipeTitleOrImageClick(props.recipe)}>{props.recipe.recipeName}</span>
       <div className="recipe-byline"><span className="recipe-byline-author">{props.recipe.sourceDisplayName}</span> </div>
-      <img src={props.recipe.imageUrl} className="recipe-image" onClick={() => props.handlerecipeTitleOrImageClick(props.recipe)}/>
+      <img height="200" src={props.recipe.imageUrl} className="recipe-image" onClick={() => props.handlerecipeTitleOrImageClick(props.recipe)}/>
       <span className="recipe-body">{props.recipe.description.split('\n').map(paragraph => <p>{paragraph}</p>)}</span>
-      <div className="recipe-stats">{props.recipe.favs} &hearts;</div>
+      <div className="recipe-stats" onClick={() => favPlusOne(props)}>{props.recipe.favs} &hearts;</div>
     </div>
   </li>
 );
