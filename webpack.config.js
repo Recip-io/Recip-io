a65fb9ededa58922214343f776600a63de0f4651
Heshie London <heshielondon@gmail.com>
2018-06-10 14:02:44 -0700

    updated for crossorigin dev env

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,12 @@ var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
+  devtool: "eval-cheap-module-source-map",
   entry: `${SRC_DIR}/App.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: DIST_DIR,
+    crossOriginLoading: "use-credentials"
   },
   module : {
     rules : [
@@ -15,7 +17,7 @@ module.exports = {
         include : SRC_DIR,
         loader : 'babel-loader',
         query: {
-          presets: ['react', 'env']
+          presets: ['react', 'env', 'es2015']
        }
       }
     ]
