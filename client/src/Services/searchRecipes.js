d8c724372792cbac49e1beeb7f94ec460fd39c47
Heshie London <heshielondon@gmail.com>
2018-06-11 16:36:01 -0700

    cleaned up code

--- client/src/Services/searchRecipes.js
+++ client/src/Services/searchRecipes.js
@@ -1,7 +1,6 @@
 import $ from 'jquery';
 
 export function searchRecipes(stateInSearch, callback) {
-  // event.preventDefault();
   console.log('in searchRecipe, and this.state = ', stateInSearch);
   $.ajax({
     method: 'POST',
@@ -10,19 +9,9 @@ export function searchRecipes(stateInSearch, callback) {
     contentType: 'application/json'
   })
     .then(function(data) {
-
-      // console.log('searchRecipes data = ', JSON.parse(data));
       callback(JSON.parse(data));
-      // let searchRes = JSON.parse(data);
-      // callback(JSON.parse(data));
-      // this.props.handleDbSearchResults(searchRes);
-      // $( ".searchdb" ).insertAfter( "<RecipeList recipes={JSON.parse(data)} handleClick={() => this.changeView('recipeview')}  componentDidMount={this.componentDidMount} handleRecipeTitleOrImageClick={this.handleRecipeTitleOrImageClick} />" );
-      // console.log('search component posted successfully', JSON.parse(data));
     })
     .fail(function() {
       console.log('search component failed to post');
   });
-  // this.passResult(searchRes);
-  // console.log('this.state.result = ', this.state.result);
-  // console.log(props.handleDbSearchResults, searchRes);
 }
