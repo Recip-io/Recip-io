b12a59ee9399e1f22d7a58ee0ba361fd2a1a1c25
Heshie London <heshielondon@gmail.com>
2018-06-10 23:37:02 -0700

    added dynamic handling of recipe based on source being local or external

--- client/src/components/Recipe.jsx
+++ client/src/components/Recipe.jsx
@@ -21,16 +21,33 @@ var favPlusOne = function(props) {
   });
 }
 
+var isYumRecipe = function(props) {
+  if (props.recipe.id) {
+    return  <li className="recipe-list-entry-yummly" key={props.recipe._id}>
+              <div className="recipe">
+                <a href={'https://www.yummly.com/recipe/' + props.recipe.id} target="_blank"><span className="recipe-title">{props.recipe.recipeName}</span></a>
+                <div className="recipe-byline"><span className="recipe-byline-author">{props.recipe.sourceDisplayName}</span></div>
+                <img height="90px" src={props.recipe.imageUrl} className="recipe-image-yummly" />
+                <div className="recipe-stats" onClick={() => favPlusOne(props)}>{props.recipe.favs} &hearts;</div>
+              </div>
+            </li>
+  } else {
+    return  <li className="recipe-list-entry" key={props.recipe._id}>
+              <div className="recipe">
+                <span className="recipe-title" onClick={() => props.handleRecipeTitleOrImageClick(props.recipe)}>{props.recipe.recipeName}</span>
+                <div className="recipe-byline"><span className="recipe-byline-author">{props.recipe.sourceDisplayName}</span></div>
+                <img height="200" src={props.recipe.imageUrl} className="recipe-image" onClick={() => props.handlerecipeTitleOrImageClick(props.recipe)}/>
+                <span className="recipe-body">{props.recipe.description.split('\n').map(paragraph => <p>{paragraph}</p>)}</span>
+                <div className="recipe-stats" onClick={() => favPlusOne(props)}>{props.recipe.favs} &hearts;</div>
+              </div>
+            </li>
+  }
+}
+
 const Recipe = (props) => (
-  <li className="recipe-list-entry" key={props.recipe._id}>
-    <div className="recipe">
-      <span className="recipe-title" onClick={() => props.handleRecipeTitleOrImageClick(props.recipe)}>{props.recipe.recipeName}</span>
-      <div className="recipe-byline"><span className="recipe-byline-author">{props.recipe.sourceDisplayName}</span></div>
-      <img height="200" src={props.recipe.imageUrl} className="recipe-image" onClick={() => props.handlerecipeTitleOrImageClick(props.recipe)}/>
-      <span className="recipe-body">{props.recipe.description.split('\n').map(paragraph => <p>{paragraph}</p>)}</span>
-      <div className="recipe-stats" onClick={() => favPlusOne(props)}>{props.recipe.favs} &hearts;</div>
-    </div>
-  </li>
+  <React.Fragment>
+    {isYumRecipe(props)}
+  </React.Fragment>
 );
 
 export default Recipe;
