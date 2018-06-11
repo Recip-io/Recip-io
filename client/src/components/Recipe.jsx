5aab280dc8efd26682e41f28027b1f514d78b1bc
Heshie London <heshielondon@gmail.com>
2018-06-11 10:15:10 -0700

    added delete function

--- client/src/components/Recipe.jsx
+++ client/src/components/Recipe.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import $ from 'jquery';
-
+import octicons from 'octicons';
 
 var favPlusOne = function(props) {
   event.preventDefault();
@@ -21,6 +21,25 @@ var favPlusOne = function(props) {
   });
 }
 
+var deleteOne = function(props) {
+  event.preventDefault();
+  var recipioId = JSON.stringify({ _id: props.recipe._id });
+  console.log('in deleteOne, recipioId = ', recipioId);
+  $.ajax({
+    method: 'POST',
+    url: '/api/recipios/delete',
+    data: recipioId,
+    contentType: 'application/json'
+  })
+    .done(function(data) {
+      console.log('recipe deleted successfully', data);
+      props.componentDidMount();
+    })
+    .fail(function() {
+      console.log('failed to delete');
+  });
+}
+
 var isYumRecipe = function(props) {
   if (props.recipe.id) {
     return  <li className="recipe-list-entry-yummly" key={props.recipe._id}>
@@ -28,7 +47,9 @@ var isYumRecipe = function(props) {
                 <a href={'https://www.yummly.com/recipe/' + props.recipe.id} target="_blank"><span className="recipe-title">{props.recipe.recipeName}</span></a>
                 <div className="recipe-byline"><span className="recipe-byline-author">{props.recipe.sourceDisplayName}</span></div>
                 <img height="90px" src={props.recipe.imageUrl} className="recipe-image-yummly" />
-                <div className="recipe-stats" onClick={() => favPlusOne(props)}>{props.recipe.favs} &hearts;</div>
+                <div><span className="recipe-stats" onClick={() => favPlusOne(props)}>{props.recipe.favs}   <span class="glyphicon glyphicon-heart"></span></span>     <span className="recipe-stats" onClick={() => deleteOne(props)}>Delete this recipe <span class="glyphicon glyphicon-trash"></span>
+                </span>
+                </div>
               </div>
             </li>
   } else {
@@ -38,7 +59,9 @@ var isYumRecipe = function(props) {
                 <div className="recipe-byline"><span className="recipe-byline-author">{props.recipe.sourceDisplayName}</span></div>
                 <img height="200" src={props.recipe.imageUrl} className="recipe-image" onClick={() => props.handlerecipeTitleOrImageClick(props.recipe)}/>
                 <span className="recipe-body">{props.recipe.description.split('\n').map(paragraph => <p>{paragraph}</p>)}</span>
-                <div className="recipe-stats" onClick={() => favPlusOne(props)}>{props.recipe.favs} &hearts;</div>
+                <div><span className="recipe-stats" onClick={() => favPlusOne(props)}>{props.recipe.favs} &hearts;</span>     <span className="recipe-stats" onClick={() => deleteOne(props)}>Delete this recipe <span class="glyphicon glyphicon-trash"></span>
+                </span>
+                </div>
               </div>
             </li>
   }
