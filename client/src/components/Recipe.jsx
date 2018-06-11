4ae3916a65e249a0f0bdc268aa68ed3f30d1fb30
Heshie London <heshielondon@gmail.com>
2018-06-10 23:03:58 -0700

    updated render

--- client/src/components/Recipe.jsx
+++ client/src/components/Recipe.jsx
@@ -25,7 +25,7 @@ const Recipe = (props) => (
   <li className="recipe-list-entry" key={props.recipe._id}>
     <div className="recipe">
       <span className="recipe-title" onClick={() => props.handleRecipeTitleOrImageClick(props.recipe)}>{props.recipe.recipeName}</span>
-      <div className="recipe-byline"><span className="recipe-byline-author">{props.recipe.sourceDisplayName}</span> </div>
+      <div className="recipe-byline"><span className="recipe-byline-author">{props.recipe.sourceDisplayName}</span></div>
       <img height="200" src={props.recipe.imageUrl} className="recipe-image" onClick={() => props.handlerecipeTitleOrImageClick(props.recipe)}/>
       <span className="recipe-body">{props.recipe.description.split('\n').map(paragraph => <p>{paragraph}</p>)}</span>
       <div className="recipe-stats" onClick={() => favPlusOne(props)}>{props.recipe.favs} &hearts;</div>
